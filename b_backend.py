97248634875fc7543abf97bd9b0879390cddc2d9
javier.manzanares <manzanaresflores90@gmail.com>
2023-10-12 23:35:45 +0200

    OpenAI project with backen, frontend and sqlite database

---
+++ b_backend.py
@@ -0,0 +1,39 @@
+# 1. Cargar la bbdd con langchain
+# from langchain.sql_database import SQLDatabase
+from langchain.utilities import SQLDatabase
+#db = SQLDatabase.from_uri("sqlite:////ecommerce.db")
+db_uri = "sqlite:////Users/javier/OpenAI-projects/Chatdatabase/ecommerce.db"
+db = SQLDatabase.from_uri(db_uri)
+
+# 2. Importar las APIs
+import a_env_vars
+import os
+os.environ["OPENAI_API_KEY"] = a_env_vars.OPENAI_API_KEY
+
+# 3. Crear el LLM
+from langchain.chat_models import ChatOpenAI
+llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
+
+# 4. Crear la cadena
+#from langchain import SQLDatabaseChain
+from langchain_experimental.sql import SQLDatabaseChain
+#cadena = SQLDatabaseChain.from_llm(llm=llm, database=db, verbose=False)
+cadena = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+
+# 5. Formato personalizado de respuesta
+formato = """
+Dada una pregunta del usuario:
+1. crea una consulta de sqlite3
+2. revisa los resultados
+3. devuelve el dato
+4. si tienes que hacer alguna aclaración o devolver cualquier texto que sea siempre en español
+#{question}
+"""
+
+# 6. Función para hacer la consulta
+def consulta(input_usuario):
+    consulta = formato.format(question=input_usuario)
+    resultado = cadena.run(consulta)
+    return resultado
+
+#cadena.run("cuantas columnas hay en la base de datos?")
---
+++ c_front_end.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import b_backend
+
+
+if 'preguntas' not in st.session_state:
+    st.session_state.preguntas = []
+
+if 'respuestas' not in st.session_state:
+    st.session_state.respuestas = []
+
+st.title("App para Usuario de Negocio")
+st.write("Puedes hacerme a mi todas las preguntas y dejar trabajar al equipo de Sistemas!!")
+
+def click():
+    if st.session_state.user != '':
+        pregunta = st.session_state.user
+        respuesta = b_backend.consulta(pregunta)
+
+        st.session_state.preguntas.append(pregunta)
+        st.session_state.respuestas.append(respuesta)
+
+        # Limpiar el input de usuario después de enviar la pregunta
+        st.session_state.user = ''
+
+with st.form('my-form'):
+    query = st.text_input('¿En qué te puedo ayudar?:', key='user', help='Pulsa Enviar para hacer la pregunta')
+    submit_button = st.form_submit_button('Enviar', on_click=click)
+
+if st.session_state.preguntas:
+    for i in range(len(st.session_state.preguntas) - 1, -1, -1):
+        message(st.session_state.preguntas[i], key=str(i))
+
+    # Opción para continuar la conversación
+    continuar_conversacion = st.checkbox('Quieres hacer otra pregunta?')
+    if not continuar_conversacion:
+        st.session_state.preguntas = []
+        st.session_state.respuestas = []
+
+
