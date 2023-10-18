5d33204ed7236695144cccb48f02357bd644b779
javier.manzanares <manzanaresflores90@gmail.com>
2023-10-18 18:21:34 +0200

    add new formato

--- b_backend.py
+++ b_backend.py
@@ -22,7 +22,8 @@ Dada una pregunta del usuario:
 1. crea una consulta de sqlite3
 2. revisa los resultados
 3. devuelve el dato
-4. si tienes que hacer alguna aclaración o devolver cualquier texto que sea siempre en español
+4. analiza y explica el resultado
+5. si tienes que hacer alguna aclaración o devolver cualquier texto que sea siempre en español
 #{question}
 """
 
